db66e740fb50b15b8686532132a8c25fc9002310
Nam Tran <nakani3797@gmail.com>
2025-11-11 15:54:19 +0700

    update title

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // Tùy chọn cho production
+  images: {
+    domains: ['images.unsplash.com'], // Cho phép image từ Unsplash
+  },
 };
 
 export default nextConfig;
